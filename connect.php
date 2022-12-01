70cf5b382747556d1da1ebaf1f77891549df4365
Ibrahimhouba <111587954+Ibrahimhouba@users.noreply.github.com>
2022-12-01 13:39:10 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=192.168.40.132 port=5432  user=caux password=caux dbname=caux");
+$conn = pg_connect("host=192.168.174.128 port=5432  user=caux password=caux dbname=caux");
 if(!$conn){
     echo "Database connection failed. Error:".$conn->error;
     exit;
